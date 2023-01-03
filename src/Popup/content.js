63c42adfed8dd795879ddc42ebf2e5e54c43618a
Tanner Lyon <tannerhlyon@gmail.com>
2023-01-03 13:11:42 -0500

    added prelim content script

--- src/Popup/content.js
+++ src/Popup/content.js
@@ -1,1 +1,9 @@
 alert('Hello world from content.js');
+
+
+
+// import { SHARED_PORT } from './messenger';
+
+// console.log("Content Script Working at PORT:", SHARED_PORT);
+
+// const port = chrome.runtime.connect({ name: SHARED_PORT })
